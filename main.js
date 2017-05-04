b26da4280a8af9584a92c52d32e8a117e4dd5a86
souelene <hola.sou.elene@gmail.com>
2017-05-04 10:52:01 -0300

    Ejercicio guiado Pokemóm!

---
+++ main.js
@@ -0,0 +1,20 @@
+function Pokemon(nombre,color,poderDeAtaque){
+	this,nombre=nombre
+	this.color=color
+	this.nivelDeAmistad=0
+	this.vida=100
+	this.poderDeAtaque=poderDeAtaque
+	this.mostrarPokemon=function(){
+		return ("hola,soy "+this.nombre+" y soy de color"+this.color)
+	}
+	this.aumentarAmistad=function(valor){
+		this.nivelDeAmistad=this.nivelDeAmistad+valor
+	}
+	this.atacar=function(pokemon){
+		pokemon.vida=pokemon.vida-this.poderDeAtaque
+	}
+}
+const Pikachu=new Pokemon("Pikachu","amarillo",100)
+const Charmander=new Pokemon("Charmander","rojo",20)
+Pikachu.atacar(Charmander)
+console.log(Charmander.vida)

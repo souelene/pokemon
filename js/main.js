9fa96e074db5ebde97e9edec76abe0e8d8a88c79
souelene <hola.sou.elene@gmail.com>
2017-05-05 13:00:37 -0300

    Se realiza pagina de estilos y se revisa funcion. aun pendiente

--- js/main.js
+++ js/main.js
@@ -16,7 +16,7 @@ function Pokemon(nombre,color,poderDeAtaque){
 }
 const Pikachu=new Pokemon("Pikachu","amarillo",100)
 const Charmander=new Pokemon("Charmander","rojo",20)
-const Raticate = new Pokemon('Raticate','cafe',90)
+const Raticate = new Pokemon('Raticate','cafe',90)	
 /*Pikachu.atacar(Charmander)
 console.log(Charmander.vida)*/
 
@@ -31,12 +31,12 @@ function batalla(nombre,color,poderDeAtaque){
 		alert("No,no,no,no... Los hermanos no pelean, la batalla no sucedera si ambos son de la misma especie")
 	}else{ 
 
-	var poderAtaque = prompt("Ingresa el poder de ataque de "+ elige1);
-	var primerPokemon = new Pokemon(elige1+color+poderAtaque);
-	var segundoPokemon = new Pokemon(elige2+color+poderDeAtaque);	
-	primerPokemon.atacar(segundoPokemon);
-}
-	
-	respuesta.innerHTML="<br>" + primerPokemon.nombre+" atacó a "+segundoPokemon.nombre+" y "+segundoPokemon.nombre+" tiene una vida de: "+segundoPokemon.vida;
+	var primerPokemon = new Pokemon(elige1,color,poderDeAtaque);
+	var segundoPokemon = new Pokemon(elige2,color,poderDeAtaque);	
+	primerPokemon.atacar(segundoPokemon)
+
+}	
+	var respuesta = document.getElementById("respuesta");
+	respuesta.innerHTML = "<p>" + elige1+" atacó a "+elige2+" y "+elige2+" tiene una vida de: "+segundoPokemon.vida+"</p>";
 
 }
